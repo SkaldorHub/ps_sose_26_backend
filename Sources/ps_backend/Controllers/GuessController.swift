d4a6743d838c4a626a8aec3e43146fb378341bde
Rafael Inderique Arnold <dummy@skaldor.de>
2026-05-27 14:31:17 +0200

    Split up APIHandler File to Controllers

---
+++ Sources/ps_backend/Controllers/GuessController.swift
@@ -0,0 +1,16 @@
+import OpenAPIRuntime
+
+extension APIHandler {
+
+    func getTeamPhoto(_ input: Operations.getTeamPhoto.Input) async throws -> Operations.getTeamPhoto.Output {
+        .undocumented(statusCode: 501, .init())
+    }
+
+    func submitGuess(_ input: Operations.submitGuess.Input) async throws -> Operations.submitGuess.Output {
+        .undocumented(statusCode: 501, .init())
+    }
+
+    func getGuesses(_ input: Operations.getGuesses.Input) async throws -> Operations.getGuesses.Output {
+        .undocumented(statusCode: 501, .init())
+    }
+}
---
+++ Sources/ps_backend/Controllers/ResultsController.swift
@@ -0,0 +1,16 @@
+import OpenAPIRuntime
+
+extension APIHandler {
+
+    func getCurrentResult(_ input: Operations.getCurrentResult.Input) async throws -> Operations.getCurrentResult.Output {
+        .undocumented(statusCode: 501, .init())
+    }
+
+    func getLeaderboard(_ input: Operations.getLeaderboard.Input) async throws -> Operations.getLeaderboard.Output {
+        .undocumented(statusCode: 501, .init())
+    }
+
+    func getGameResult(_ input: Operations.getGameResult.Input) async throws -> Operations.getGameResult.Output {
+        .undocumented(statusCode: 501, .init())
+    }
+}
