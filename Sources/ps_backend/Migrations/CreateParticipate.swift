ff601f6da6c367190b7c5da6395b4aadeaad188a
Aaron Tewelde <aaron.tewelde@student.htw-berlin.de>
2026-05-31 06:37:35 +0200

    renamed classes for consistency

---
+++ Sources/ps_backend/Migrations/CreateParticipate.swift
@@ -0,0 +1,22 @@
+import Fluent   
+
+/// Migration to create the "participate" table in the database, linking teams to games and indicating whether they are winners
+struct CreateParticipate: AsyncMigration {    
+
+    // Creates the "participate" table with the specified fields and relationships
+    func prepare(on database: Database) async throws {
+        try await database.schema(Participate.schema)
+            .id()
+            .field("game_id", .uuid, .required, .references(Game.schema, "id", onDelete: .restrict))
+            .field("team_id", .uuid, .required, .references(Team.schema, "id", onDelete: .restrict))
+            // combination of game_id and team_id must be unique to prevent duplicate entries for the same team in the same game
+            .unique(on: "game_id", "team_id") 
+            .field("is_winner", .bool, .required, .sql(.default(false)))
+            .create()
+    }
+
+    // Deletes the "participate" table if the migration is reverted
+    func revert(on database: Database) async throws {
+        try await database.schema(Participate.schema).delete()
+    }
+}
