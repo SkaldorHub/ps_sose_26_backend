9277c5683650d60afe4facf4d4e548b371fb7404
ElKekso <159456154+ElKekso@users.noreply.github.com>
2026-06-05 11:57:45 +0200

    Merge current into Auth Branch

--- Sources/ps_backend/Controllers/AuthController.swift
+++ Sources/ps_backend/Controllers/AuthController.swift
@@ -9,7 +9,7 @@ extension APIHandler {
     }
 
     // Content Structs for return needed? mby Operations.login.Output??
-    func login(req: Request) async throws -> Operations.login.Output {
+    func login(_ input: Operations.login.Input) async throws -> Operations.login.Output {
         .undocumented(statusCode: 501, .init())
     }
 
@@ -18,14 +18,22 @@ extension APIHandler {
     }
 }
 
-/*
-
-Payload needed -> in Model?
-
 struct AuthPayload: JWTPayload {
+    // Maps the longer Swift property names to the
+    // shortened keys used in the JWT payload.
+    enum CodingKeys: String, CodingKey {
+        case subject = "sub"
+        case expiration = "exp"
+        case username = "usr"
+    }
 
-    //What information needs to be transmitted?
+    var subject: SubjectClaim
+    
+    var expiration: ExpirationClaim
 
+    var username: String
+    
+    func verify(using algorithm: some JWTAlgorithm) async throws {
+        try self.expiration.verifyNotExpired()
+    }
 }
-
-*/
