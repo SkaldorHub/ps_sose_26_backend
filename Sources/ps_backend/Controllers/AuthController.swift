dee2d3726b9b8559d3949af08085344524dc8708
ElKekso <159456154+ElKekso@users.noreply.github.com>
2026-06-12 15:19:35 +0200

    Write Endpoints

--- Sources/ps_backend/Controllers/AuthController.swift
+++ Sources/ps_backend/Controllers/AuthController.swift
@@ -2,6 +2,7 @@ import OpenAPIRuntime
 import JWT
 import Vapor
 import Foundation
+import Fluent
 
 
 extension APIHandler {
@@ -9,49 +10,74 @@ extension APIHandler {
     //input eq req object
     func register(_ input: Operations.register.Input) async throws -> Operations.register.Output {
 
-        var id: UUID
-        var username = input.username
-        var password = input.password
-        var user: User
-        //check if username & password are valid
-        if(true) {
+        let id: UUID
+        let username: String = input.body.
+        let password: String = input.password
+        let user: User
+        let regex: String = "^([A-Za-z0-9])+$"
+        let db: Database = req.db
+
+        let valid = validate(text: username, with: regex) && validate(text: password, with: regex)
+
+        //check if username & password are valid?
+        
+        if(valid) {
             password = try input.password.hash(password)
-            try {
-                //create user in database
+            do {
+                user = User(username: username, passwordHash: password)
+                user.create(on: db)
+                id = user.id!
             } catch {
-                Abort(409)
+                throw Abort(/* db error mb rather internal? */)
             }
         } else {
-            Abort(400)
+            Abort(/* validation error */)
         }
 
-        let user = //get user from database
-
         let payload = AuthPayload(
-            subject: user.id
-            expiration: getDate()
+            subject: .init(value: id.description),
+            expiration: .init(value: getDate()!),
             username: user.username
         )
+
+        let token = try await req.jwt.sign(payload)
         
-        return try await ["token": input.jwt.sign(payload)]
+        return .created(.init(body: .json(.init(
+            token: token,
+            userId: id.description,
+            username: username
+        ))))
     }
 
     // Content Structs for return needed? mby Operations.login.Output??
     func login(_ input: Operations.login.Input) async throws -> Operations.login.Output {
         //check if username and password are correct
-        let passwordHash = //get passwordHash from Database for username
-        let correct = try input.password.verify(input.password, passwordHash)
+        let db: Database = req.db
+        let username: String = input.username
+        let password: String = input.password
+
+        let user: User? = User.query(on: db).filter(\.$username == username).first()
+
+        let correct = try req.password.verify(input.password, user!.passwordHash)
+
         if(correct) {
-            let user = // get User from Database
-            let payload = AuthPayload(
-            subject: user.id
-            expiration: getDate()
-            username: user.username
+
+            let id = user!.id!
+            let payload: AuthPayload = AuthPayload(
+            subject: .init(value: id.description),
+            expiration: .init(value: getDate()!),
+            username: user!.username
             )
+
+            let token = try await ["token": input.jwt.sign(payload)]
         
-            return try await ["token": input.jwt.sign(payload)]
+            return .ok(.init(body: .json(.init(
+            token: token,
+            userId: id.description,
+            username: username
+        ))))
         } else {
-            Abort(401)
+            Abort(/* Login error */)
         }
     }
 
@@ -96,5 +122,22 @@ func getDate() -> Date? {
     let date = userCalender.date(from: dc)
 
     return date
+}
+
+func validate(text: String, with regex: String) -> Bool {
+        // Create the regex
+        guard let gRegex = try? NSRegularExpression(pattern: regex) else {
+            return false
+        }
+        
+        // Create the range
+        let range = NSRange(location: 0, length: text.utf16.count)
+        
+        // Perform the test
+        if gRegex.firstMatch(in: text, options: [], range: range) != nil {
+            return true
+        }
+        
+        return false
 }
 
